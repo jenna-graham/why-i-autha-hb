88e3fb049b942630fcfdd952bbf8fb513082a796
jenna-graham <jclayto2@gmail.com>
2022-05-16 11:34:12 -0700

    set up sign up event listener and fetch utils for sign up

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
+// const signInForm = document.getElementById('sign-in');
+// const signInEmail = document.getElementById('sign-in-email');
+// const signInPassword = document.getElementById('sign-in-password');
+
+import { signupUser } from './fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
+// const signUpEmail = document.getElementById('sign-up-email');
+// const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+signUpForm.addEventListener('submit', async (e) =>{
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log({ email: data.get('email'), password: data.get('password') });
+    const user = await signupUser(data.get('email'), data.get('password'));
+    console.log(user);
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://icyrbwltlybmigeljxmh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeXJid2x0bHlibWlnZWxqeG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDMxMzMsImV4cCI6MTk2Nzg3OTEzM30.kUzjWO4wi0A9AVcWdFt_BG9uq-HBZoAR2aZ4IRN55yw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,12 +8,20 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }
+    
+}
 
-export async function signInUser(email, password) {}
+// export async function signInUser(email, password) {}
 
-export async function checkAuth() {}
+// export async function checkAuth() {}
 
-export async function redirectIfLoggedIn() {}
+// export async function redirectIfLoggedIn() {}
 
-export async function logout() {}
+// export async function logout() {}
